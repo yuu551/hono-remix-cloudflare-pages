28882591204b3729998467c8a1f822d1245bc73b
Yudai Jinno <jinno.yudai@classmethod.jp>
2024-09-22 13:17:56 +0900

    cors修正

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { cors } from "hono/cors";
 
 const app = new Hono();
-app.use("/api/*", cors());
+app.use("*", cors());
 
 app.get("/api", (c) => c.json("Hello"));
 
